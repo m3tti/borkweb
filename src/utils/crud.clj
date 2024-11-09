7ff4481455802881b9cef24844bfeb06d4cbdb50
Mathaeus Sander <mathaeus@mathaeus-sander.de>
2024-11-09 14:14:44 +0100

    updated behaviour crud.clj

--- src/utils/crud.clj
+++ src/utils/crud.clj
@@ -14,18 +14,40 @@
       (r/flash-msg (r/redirect redirect-path)
                    "danger" "Couldn't delete!"))))
 
-(defn update!
-  [& {:keys [req update-fn insert-fn normalized-data
+(defn create!
+  [& {:keys [req create-fn normalized-data
              redirect-path does-already-exist?]
       :or {does-already-exist? false}}]
-  (try
-    (if (get-in req [:params "id"])
-      (update-fn normalized-data)
-      (if does-already-exist?
-        (throw (Exception. "Item already exists!"))
-        (insert-fn normalized-data)))
-    (r/redirect redirect-path)
+  (try 
+    (if does-already-exist?
+      (throw (Exception. "Item already exists!"))
+      (do
+        (create-fn normalized-data)
+        (r/redirect redirect-path)))
     (catch Exception e
       (log/error e)
       (r/flash-msg (r/redirect redirect-path)
                    "danger" (e/stacktrace->str e)))))
+
+(defn update!
+  [& {:keys [req update-fn normalized-data
+             redirect-path]}]
+  (try
+    (if (get-in req [:params "id"])
+      (do 
+        (update-fn normalized-data)
+        (r/redirect redirect-path))
+      (throw (Exception. "Item is new could not update!")))
+    (catch Exception e
+      (log/error e)
+      (r/flash-msg (r/redirect redirect-path)
+                   "danger" (e/stacktrace->str e)))))
+
+(defn upsert!
+  [& {:keys [req create-fn update-fn normalized-data
+             redirect-path does-already-exist?]
+      :or {does-already-exist? false}
+      :as opts}]
+  (if (get-in req [:params "id"])
+    (update! opts)
+    (create! opts)))
