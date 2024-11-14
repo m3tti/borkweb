230c212fdd61ce0012c614e0c6a27ae703253fe1
Mathaeus Sander <mathaeus@mathaeus-sander.de>
2024-11-14 16:10:05 +0100

    adds more advanced file upload

--- resources/cljs/base64-upload.cljs
+++ resources/cljs/base64-upload.cljs
@@ -1,10 +1,11 @@
 (ns base64-upload)
 
 (defn upload [id ev]
-  (let [reader (js/FileReader.)]
+  (let [reader (js/FileReader.)
+        filename (.-name (first ev.files))]
     (.readAsBinaryString reader (first ev.files))
     (set! reader.onload (fn [ev] 
                           (set! (.-value (js/document.getElementById id)) 
-                                (js/btoa ev.target.result))))))
+                                (str filename ";" (js/btoa ev.target.result)))))))
 
 (set! js/window.base64_upload upload)
---
+++ src/utils/base64-upload.clj
@@ -0,0 +1,12 @@
+(ns utils.base64-upload
+  (:require 
+   [utils.encode :as encode]
+   [clojure.string :as str]))
+
+(defn get-file 
+  [req id path]
+  (let* [file-data (str/split (get-in req [:params id]) #";")
+         filename (first file-data)
+         b64 (second file-data)]
+    {:name filename
+     :file (encode/decode->file b64 (str path filename))}))
